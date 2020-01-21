0de80bd288904dbcee5d531b5895a42920da5103
Nicolas Viveros Barrera <nviverosb@developmen04neiaco.local>
2020-01-21 17:11:05 -0500

    Step 3 - Make a Command Line Executable

---
+++ lib/issues/cli.ex
@@ -0,0 +1,78 @@
+defmodule Issues.CLI do
+    
+    import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
+
+    @default_count 4
+    @headers ["num", "created_at", "title"]
+
+    @moduledoc """
+        Handle the command line parsing and the dispatch to
+        the various functions that end up generating a
+        table of the last _n_ issues in a github project
+    """
+
+    def main(argv) do
+        argv
+            |> parse_args
+            |> process
+    end
+
+    @doc """
+        `argv` can ve -h or --helo, which returns :help
+
+        Otherwise it is a github user name, project name, and (optionally)
+        the number of entries to format
+
+        Return a tuple of `{ user, project, count }`, or `:help` if help was given
+    """
+
+    def parse_args(argv) do
+        OptionParser.parse(argv, switches: [help: :boolean],
+                            aliases: [h: :help])
+        |> elem(1)
+        |> args_to_internal_representation()
+    end
+    def args_to_internal_representation([user, project, count]) do
+        { user, project, String.to_integer(count) }    
+    end
+    def args_to_internal_representation([user, project]) do
+        { user, project, @default_count }
+    end
+    def args_to_internal_representation(_) do
+        :help
+    end
+
+
+    def process(:help) do
+        IO.puts """
+            usage: issues <user> <project> [count | #{@default_count}]
+        """
+        System.halt
+    end
+    def process({user, project, _count}) do
+        Issues.GithubIssues.fetch(user, project)
+        |> decode_response()
+        |> sort_into_descending_order()
+        |> last(_count)
+        |> print_table_for_columns(["number","created_at","title"])
+    end
+
+    def sort_into_descending_order(list_of_issues) do
+        list_of_issues
+            |> Enum.sort(fn i1, i2 -> 
+                i1["created_at"] >= i2["created_at"]
+            end)
+    end
+
+    def decode_response({:ok, body}), do: body
+    def decode_response({:error, error}) do
+        IO.puts "Error fetching from Github: #{error["message"]}"
+        System.halt(2)
+    end
+
+    def last(list, count) do
+        list
+            |> Enum.take(count)
+            |> Enum.reverse
+    end
+end
