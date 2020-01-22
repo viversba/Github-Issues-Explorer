b5ed3a8caf28ebddd4353fdfa92df09392238050
Nicolas Viveros Barrera <nviverosb@developmen04neiaco.local>
2020-01-22 11:48:11 -0500

    Documentation added

--- mix.exs
+++ mix.exs
@@ -10,6 +10,13 @@ defmodule Issues.MixProject do
       name: "Github Issues Explorer",
       source_url: "https://github.com/viversba/Github-Issues-Explorer",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -26,8 +33,9 @@ defmodule Issues.MixProject do
     [
       {:httpoison, "~> 1.0.0"},
       {:poison, "~> 3.1"},
-      {:ex_doc, "~> 0.18.1"},
-      {:earmark, "~> 1.2.4"}
+      {:ex_doc, "~> 0.21"},
+      {:earmark, "~> 1.3"},
+      {:excoveralls, "~> 0.5.5", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
---
+++ test/doc_test.exs
@@ -0,0 +1,4 @@
+defmodule DoctTest do
+    use ExUnit.Case
+    doctest Issues.TableFormatter
+end
